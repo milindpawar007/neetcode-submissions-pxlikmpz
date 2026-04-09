b2dacebe19746ac6f1ce6f215aefd5d50b3173f2
Milind Pawar <milindpawar5555@gmail.com>
2026-04-09 17:59:40 -0500

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) 
+    {
+           int maxprofit=0;
+           int minprice= prices[0];
+           for(int i=1;i<prices.length;i++)
+           {
+            minprice=Math.min(minprice,prices[i]);
+
+            maxprofit=Math.max(maxprofit,prices[i]-minprice);
+               
+
+           }   
+
+           return maxprofit; 
+    }
+}
